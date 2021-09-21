3a38865568ab2839cc7d57be063bdf4da7825d5f
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-09-21 22:17:18 +0530

    input in c++

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int records;
+    cout << "Enter the no of records: ";
+    cin >>records;
+    cout << "Book records are "<< records << endl;
+
+    string bookname;
+    cout<< "Enter the bookname: ";
+    cin >> bookname;
+    cout <<" Book name is "<<bookname;
+    return 0;
+
+}
