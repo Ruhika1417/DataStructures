1a16fde44adc6314ea1660c3b8fe3eefc4868e9a
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-10-01 14:07:58 +0530

    Reversing a string using for loop & length() fun

--- Strings/reverse_string.cpp
+++ Strings/reverse_string.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
+string reverse;
+int main(){
+    string str=" Ruhika";
+    for (int i=str.length();i>=1;i--){
+    reverse+=str[i];
+    }
+    cout<<" reverse string is : "<<reverse;
 
+    return 0;
+}
