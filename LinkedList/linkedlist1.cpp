56fb433ffaaabf9947f9f6058cf57f911e0ef63e
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-11-19 23:55:50 +0530

    linkedlist with deletion function..

--- LinkedList/linkedlist1.cpp
+++ LinkedList/linkedlist1.cpp
@@ -1,32 +1,48 @@
-//  node consists of next(address) and data(val);
+//  node consists of next(address) and data(val); 
 
 #include<iostream>
 using namespace std;
 //1.define variables
 class node{
-    public: 
-       node* next;  //pointer next pointing to another node
-       int data;
+    public:
+     int data; 
+       node* next;  //pointer next (pointing to another node)-type
+       
 
 
-node (int val){
+node (int val){   //constructor
     data=val;
     next=NULL;
 }
 };
 
+//linkedlist 1 2 // to :add 3
+//*so head at 1*,temp will be at 2 ,after 2 null 
+
+void insertathead(node * head, int val){
+    node * n= new node(int val);
+    node*temp=head;
+    while( temp->next!=NULL){
+        n->next=head;
+        head=n;
+    }
+}
+
+
+
 void insertail(node* &head, int val ){     //pass by reference  head coz we want to modify our linkedlist
-    // create a node                            
+    // create a node  n                          
    node* n=new node(val);
 
    if(head==NULL)
    {
-    head=n;
+    head=n;  // node  n is assigned to head // head will point to n next meah automaticallly its null
     return;
    }
-
+// store the old value of head pointer  
    node* temp=head;
    while(temp->next!=NULL){
+ // Change temp pointer to point to next node
        temp=temp->next;
    }
     temp->next=n;
@@ -69,6 +85,59 @@ bool search (node* head, int key){
 }
 
 
+void deleteathead(node* &head, int val){
+
+node* todelete=head;
+head=head->next;
+delete todelete;
+
+}
+
+
+
+
+
+//deletion //will give error if we delete first node 
+void deletion(node* head, int val){
+
+
+    if (head=NULL){
+        return;
+    }
+    
+    if (head->next=NULL){
+        deleteathead(head);
+        return;
+    }
+
+    
+    node*temp=head;
+    while(temp->next->data!=NULL){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next>next;
+  
+    delete todelete;
+}
+
+class node{
+    public:
+    int val;
+    node*previous=NULL;
+    node*current=head;
+    node*nextptr;
+
+while(current!=NULL){
+    next=current->next;
+    current->next=previous;
+    previous=current;
+    current=nextptr;
+}
+return previous;
+}
+
+
 
 int main(){
 
@@ -78,11 +147,11 @@ insertail(head,50);
 insertail(head,90);
 new_node(head,66);
 display(head);
+//deletion(head,10)
+//deleteathead(head,10)
+
 
 cout<<search(head,50)<<endl;
-
-
-
 
 return 0;
 }
